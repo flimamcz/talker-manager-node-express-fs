388d41609ffd7ad76a56befd9adacfe4a6858a9d
Filipe Lima <cimofelipe4@gmail.com>
2023-01-23 21:59:03 -0300

    feat: Requirement 2 :check: + tests camada services and controller

---
+++ tests/unit/controllers/products.controller.test.js
@@ -0,0 +1,29 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+
+const { expect } = chai;
+chai.use(sinonChai);
+
+const {productsController} = require('../../../src/controllers')
+const { productsServices } = require('../../../src/services')
+
+const { allProducts } = require('./mocks/products.controller.mock')
+
+describe('Testando a camada controller', function () {
+  it('Lista todos os produtos na rota /products', async function () {
+    const req = {};
+    const res = {};
+    res.status = sinon.stub().returns(res)
+    res.json = sinon.stub().returns()
+    sinon.stub(productsServices, 'getAllProducts').resolves({type: null, message: allProducts});
+    await productsController.getAllProducts(req, res);
+
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith(allProducts);
+  });
+
+  afterEach(function () {
+    sinon.restore();
+  })
+})
---
+++ tests/unit/services/products.servicesl.test.js
@@ -0,0 +1,29 @@
+const { expect } = require("chai");
+const sinon = require("sinon");
+
+const { allProducts, oneProduct } = require("./mocks/products.services.mock");
+const { productsServices } = require("../../../src/services/");
+
+const { productsModel } = require("../../../src/models");
+
+describe("Testando camada services", function () {
+  it("Listando todos os produtos", async function () {
+    sinon.stub(productsModel, "getAllProducts").resolves(allProducts);
+
+    const result = await productsServices.getAllProducts();
+
+    expect(result.message).to.be.deep.equal(allProducts);
+  });
+
+  it("Listando produto por ID", async function () {
+    sinon.stub(productsModel, "getProductById").resolves(oneProduct);
+
+    const result = await productsServices.getProductById(2);
+
+    expect(result.message).to.be.deep.equal(oneProduct);
+  });
+
+  afterEach(function () {
+    sinon.restore();
+  });
+});
